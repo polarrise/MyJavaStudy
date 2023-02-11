0cbe80888a44e9bfff630d88c29c900ff39b0b52
polarrise <polarrise>
2023-02-12 02:02:16 +0800

    看视频深入了解HashMap的put元素和get元素的底层原理, 一维数组+二维链表 以及红黑树的转变，数组扩容的情况等等

---
+++ src/main/java/com/jinbiao/javaStudy/collections/LearnHashMap.java
@@ -0,0 +1,16 @@
+package com.jinbiao.javaStudy.collections;
+
+import java.util.HashMap;
+
+/**
+ * @author WangJinbiao
+ * @date 2023/2/11 13:18
+ * @desc
+ */
+public class LearnHashMap {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("a",1);
+        System.out.println( hashMap.get("a"));
+    }
+}
