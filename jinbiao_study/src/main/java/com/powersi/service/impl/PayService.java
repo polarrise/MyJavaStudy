fa5abff46f4a6034ba371940a1caac61af4c37e3
polarrise <1585914314@qq.com>
2024-09-04 11:16:55 +0800

    侦听ContextRefreshedEvent事件，添加注解bean到map中存在被清空的情况，没搞懂

--- jinbiao_study/src/main/java/com/powersi/service/impl/PayService.java
+++ jinbiao_study/src/main/java/com/powersi/service/impl/PayService.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,14 +14,26 @@ import java.util.Map;
 @Service
 public class PayService implements ApplicationListener<ContextRefreshedEvent> {
 
-  private static Map<String, IPay> payMap = null;
+  // private static Map<String, IPay> payMap = null;  之前生产遇到过根据code来获取对应实现为null的情况，没搞懂payMap为什么会被清空？
+
+  private static Map<String, IPay> payMap = new HashMap<>(16);
+
   @Override
   public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
     ApplicationContext applicationContext = contextRefreshedEvent.getApplicationContext();
     Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(PayCode.class);
 
+    /**
+     * 之前生产这种写法,payMap初始定义为null,遇到过根据code来获取对应实现为null的情况，没搞懂payMap为什么会被清空？
     if (beansWithAnnotation != null) {
-      payMap = new HashMap<>();
+        payMap = new HashMap<>();
+        beansWithAnnotation.forEach((key, value) ->{
+          String bizType = value.getClass().getAnnotation(PayCode.class).value();
+          payMap.put(bizType, (IPay) value);
+        });
+    }**/
+
+    if (!CollectionUtils.isEmpty(beansWithAnnotation)) {
       beansWithAnnotation.forEach((key, value) ->{
         String bizType = value.getClass().getAnnotation(PayCode.class).value();
         payMap.put(bizType, (IPay) value);
